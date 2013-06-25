9a96a22c3737c8c1938cd52e25157bb456ec8a57
Hana Lee <hanalee07@gmail.com>
2013-06-25 18:14:46 -0500

    Tweaked subtitle again

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Hana Lee'
 SITENAME = u'Genetic Cartography'
-SITESUBTITLE = u'website and blog by H.N. Lee'
+SITESUBTITLE = u'website and blog by Hana Lee'
 SITEURL = u'http://hanalee.info'
 TIMEZONE = u'America/Chicago'
 
