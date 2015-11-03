0d6d67b1c5277f1a9bf94babaca11d33e1f184ad
Hana Lee <hanalee07@gmail.com>
2015-11-03 14:47:36 -0600

    Changed settings to work with new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,22 +4,21 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Hana Lee'
 SITENAME = u'Hana Lee'
-#SITESUBTITLE = u''
 SITEURL = u'http://hanalee.info'
 
 PATH = u'content/'
-
 TIMEZONE = u'America/Chicago'
-
 DEFAULT_LANG = u'en'
 
 MD_EXTENSIONS = ['codehilite(css_class=highlight)','extra','tables']
-THEME = "./built-texts"
+THEME = "./svbhack"
 
-DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = True
 USE_FOLDER_AS_CATEGORY = True
-ARTICLE_URL='{category}/{slug}.html'
-ARTICLE_SAVE_AS='{category}/{slug}.html'
+ARTICLE_URL = '{category}/{slug}.html'
+ARTICLE_SAVE_AS = '{category}/{slug}.html'
+DEFAULT_PAGINATION = False
+STATIC_PATHS = ['static/']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -32,19 +31,19 @@ AUTHOR_FEED_RSS = None
 #LINKS =  (('', '#'),)
 
 # Social widget
-SOCIAL = (('twitter', 'http://twitter.com/lee_hn'),
+SOCIAL = (('envelope', 'mailto:hanalee07@gmail.com'),
+          ('twitter', 'http://twitter.com/lee_hn'),
           ('github', 'http://github.com/hnlee'),
           ('linkedin', 'http://www.linkedin.com/in/hanalee07'),
           ('google scholar', 'http://scholar.google.com/citations?hl=en&user=dOVsa18AAAAJ'),)
 EMAIL = 'hanalee07@gmail.com'
 GITHUB_URL = 'http://github.com/hnlee/'
 
-# Colophon
-COLOPHON = True
-COLOPHON_TITLE = 'Navigation'
-COLOPHON_CONTENT = ""
+# Theme-specific
 
-DEFAULT_PAGINATION = False
+TAGLINE = 'scientific computing & big data'
+INTERNET_DEFENSE_LEAGUE = True
+USER_LOGO_URL = SITEURL + '/static/images/logo.png'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
